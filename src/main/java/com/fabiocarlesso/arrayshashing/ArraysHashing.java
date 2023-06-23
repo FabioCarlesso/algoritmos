5798ab238b2a10152ddc60869693dfdda513389d
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-06-23 14:55:57 -0300

    feat: Add replace elements with greatest element on right side from array int method

--- src/main/java/com/fabiocarlesso/arrayshashing/ArraysHashing.java
+++ src/main/java/com/fabiocarlesso/arrayshashing/ArraysHashing.java
@@ -39,4 +39,17 @@ public class ArraysHashing {
         return ans;
     }
 
+    public static int[] replaceElementsOnRightSideFromArrayInt(int[] arr) {
+        int n = arr.length;
+        int max = -1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[i];
+            arr[i] = max;
+            max = Math.max(max, temp);
+        }
+
+        return arr;
+    }
+
 }
--- src/main/java/com/fabiocarlesso/mainexecution/ArraysHashingMain.java
+++ src/main/java/com/fabiocarlesso/mainexecution/ArraysHashingMain.java
@@ -5,6 +5,8 @@ import com.fabiocarlesso.arrayshashing.ArraysHashing;
 import java.util.Arrays;
 
 public class ArraysHashingMain {
+    private static final String ARRAY_TEXT = "Array: ";
+
     private ArraysHashingMain() {
         throw new IllegalStateException("Utility class");
     }
@@ -13,6 +15,7 @@ public class ArraysHashingMain {
         arrayIntContainsDuplicateMain();
         isAnagramMain();
         getArrayIntConcatenationMain();
+        replaceElementsOnRightSideFromArrayIntMain();
     }
 
     private static void isAnagramMain() {
@@ -33,11 +36,11 @@ public class ArraysHashingMain {
     private static void arrayIntContainsDuplicateMain() {
         System.out.println("\n\nArray Int Contains Duplicate");
         int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println("Array: " + Arrays.toString(numbers));
+        System.out.println(ARRAY_TEXT + Arrays.toString(numbers));
         System.out.println("No duplicates: " + ArraysHashing.containsArrayIntDuplicate(numbers));
 
         int[] numbersWithDuplicate = {1, 2, 3, 2, 4, 5};
-        System.out.println("\nArray: " + Arrays.toString(numbersWithDuplicate));
+        System.out.println("\n" + ARRAY_TEXT + Arrays.toString(numbersWithDuplicate));
         System.out.println("Contains Duplicate: " + ArraysHashing.containsArrayIntDuplicate(numbersWithDuplicate));
     }
 
@@ -46,8 +49,22 @@ public class ArraysHashingMain {
         int[] nums = {1, 2, 3};
         int[] concatenated = ArraysHashing.getArrayIntConcatenation(nums);
 
-        System.out.println("Array: " + Arrays.toString(nums));
+        System.out.println(ARRAY_TEXT + Arrays.toString(nums));
         System.out.println("Concatenation: " + Arrays.toString(concatenated));
 
     }
+
+    private static void replaceElementsOnRightSideFromArrayIntMain() {
+        System.out.println("\n\nArray Int Replace Elements On Right Side");
+        int[] arr = {17, 18, 5, 4, 6, 1};
+        System.out.println(ARRAY_TEXT + Arrays.toString(arr));
+        int[] replaced = ArraysHashing.replaceElementsOnRightSideFromArrayInt(arr);
+                System.out.println("Replace Elements: " + Arrays.toString(replaced));
+
+        int[] arrTest = {400};
+        System.out.println("\n" + ARRAY_TEXT + Arrays.toString(arrTest));
+        int[] replacedError = ArraysHashing.replaceElementsOnRightSideFromArrayInt(arrTest);
+        System.out.println("Replace Elements error: " + Arrays.toString(replacedError));
+    }
+
 }
