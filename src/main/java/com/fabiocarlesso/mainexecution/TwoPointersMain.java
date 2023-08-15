67bb7f28a761177fe6dffc9bb4dd593e48200bf9
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-08-15 14:50:37 -0300

    feat(TwoPointers): Add Minimum Difference Between Highest and Lowest of K Scores method.

--- src/main/java/com/fabiocarlesso/mainexecution/TwoPointersMain.java
+++ src/main/java/com/fabiocarlesso/mainexecution/TwoPointersMain.java
@@ -1,6 +1,9 @@
 package com.fabiocarlesso.mainexecution;
 
+import com.fabiocarlesso.arrayshashing.ArraysHashing;
 import com.fabiocarlesso.twopointers.TwoPointers;
+
+import java.util.Arrays;
 
 public class TwoPointersMain {
     private static final String ARRAY_TEXT = "Array: ";
@@ -14,6 +17,7 @@ public class TwoPointersMain {
         System.out.println("\n\nTwo Pointers");
         isPalindromeMain();
         validPalindromeMain();
+        minimumDifferenceMain();
     }
     private static void isPalindromeMain() {
         System.out.println("\n\nValid Palindrome");
@@ -39,6 +43,20 @@ public class TwoPointersMain {
         phrase = "abc";
         System.out.println(ARRAY_TEXT + phrase);
         System.out.println(RESULT_CASE_3 + TwoPointers.validPalindrome(phrase));
+    }
+
+    private static void minimumDifferenceMain() {
+        System.out.println("\n\nMinimum Difference Between Highest and Lowest of K Scores");
+        int[] nums = {90};
+        int k = 1;
+        System.out.println(ARRAY_TEXT + Arrays.toString(nums));
+        System.out.println("K: " + k);
+        System.out.println(RESULT_CASE_1 + TwoPointers.minimumDifference(nums, k));
+        nums = new int[] {9,4,1,7};
+        k = 2;
+        System.out.println(ARRAY_TEXT + Arrays.toString(nums));
+        System.out.println("K: " + k);
+        System.out.println(RESULT_CASE_2 + TwoPointers.minimumDifference(nums, k));
     }
 
 }
--- src/main/java/com/fabiocarlesso/twopointers/TwoPointers.java
+++ src/main/java/com/fabiocarlesso/twopointers/TwoPointers.java
@@ -1,5 +1,7 @@
 package com.fabiocarlesso.twopointers;
 
+import java.util.Arrays;
+
 public class TwoPointers {
 
     private TwoPointers() {
@@ -50,6 +52,17 @@ public class TwoPointers {
                 return false;
             }
         return true;
+    }
+
+    public static int minimumDifference(int[] nums, int k) {
+        if(k == 1) return 0;
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<nums.length-k+1; i++) {
+            int diff = Math.abs(nums[i] - nums[i+k-1]);
+            min = Math.min(diff, min);
+        }
+        return min;
     }
 
 }
