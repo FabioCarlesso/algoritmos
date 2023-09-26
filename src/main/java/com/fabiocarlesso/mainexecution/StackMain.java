e9077f05944441121d38becb5402babf0712f84c
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-09-26 11:34:44 -0300

    feat(Stack): Add Valid Parentheses method

--- src/main/java/com/fabiocarlesso/mainexecution/StackMain.java
+++ src/main/java/com/fabiocarlesso/mainexecution/StackMain.java
@@ -1,12 +1,33 @@
 package com.fabiocarlesso.mainexecution;
 
+import com.fabiocarlesso.stack.StackFunctions;
+import com.fabiocarlesso.twopointers.TwoPointers;
+
 public class StackMain {
+    private static final String ARRAY_TEXT = "Array: ";
+    public static final String RESULT_CASE_1 = "Result case 1: ";
+    public static final String RESULT_CASE_2 = "Result case 2: ";
+    public static final String RESULT_CASE_3 = "Result case 3: ";
 
     private StackMain() {
         throw new IllegalStateException("Utility class");
     }
     public static void stackMain() {
         System.out.println("\n\nStack");
+        isValidParentheses();
+    }
+
+    private static void isValidParentheses() {
+        System.out.println("\nValid Parentheses");
+        String parentheses = "()";
+        System.out.println(ARRAY_TEXT + parentheses);
+        System.out.println(RESULT_CASE_1 + StackFunctions.isValid(parentheses));
+        parentheses = "()[]{}";
+        System.out.println(ARRAY_TEXT + parentheses);
+        System.out.println(RESULT_CASE_2 + StackFunctions.isValid(parentheses));
+        parentheses = "(]";
+        System.out.println(ARRAY_TEXT + parentheses);
+        System.out.println(RESULT_CASE_3 + StackFunctions.isValid(parentheses));
     }
 
 }
--- src/main/java/com/fabiocarlesso/stack/Stack.java
+++
@@ -1,8 +0,0 @@
-package com.fabiocarlesso.stack;
-
-public class Stack {
-
-    private Stack() {
-        throw new IllegalStateException("Utility class");
-    }
-}
