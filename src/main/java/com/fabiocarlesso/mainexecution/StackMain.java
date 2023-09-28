370732451157c3dd7a03886ed31b0002fdb30907
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-09-28 16:56:34 -0300

    feat(Stack): Add Evaluate Reverse Polish Notation

--- src/main/java/com/fabiocarlesso/mainexecution/StackMain.java
+++ src/main/java/com/fabiocarlesso/mainexecution/StackMain.java
@@ -3,6 +3,8 @@ package com.fabiocarlesso.mainexecution;
 import com.fabiocarlesso.stack.MinStack;
 import com.fabiocarlesso.stack.StackFunctions;
 
+import java.util.Arrays;
+
 public class StackMain {
     private static final String ARRAY_TEXT = "Array: ";
     public static final String RESULT_CASE_1 = "Result case 1: ";
@@ -16,6 +18,7 @@ public class StackMain {
         System.out.println("\n\nStack");
         isValidParentheses();
         minStack();
+        evalRPN();
     }
 
     private static void isValidParentheses() {
@@ -51,6 +54,19 @@ public class StackMain {
 
         int minStackResult3 = minStack.getMin();
         System.out.println(RESULT_CASE_3 + minStackResult3);
+    }
+
+    private static void evalRPN() {
+        System.out.println("\nEvaluate Reverse Polish Notation");
+        String[] tokens = new String[]{"2","1","+","3","*"};
+        System.out.println(ARRAY_TEXT + Arrays.toString(tokens));
+        System.out.println(RESULT_CASE_1 + StackFunctions.evalRPN(tokens));
+        tokens = new String[]{"4","13","5","/","+"};
+        System.out.println(ARRAY_TEXT + Arrays.toString(tokens));
+        System.out.println(RESULT_CASE_2 + StackFunctions.evalRPN(tokens));
+        tokens = new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        System.out.println(ARRAY_TEXT + Arrays.toString(tokens));
+        System.out.println(RESULT_CASE_3 + StackFunctions.evalRPN(tokens));
     }
 
 }
--- src/main/java/com/fabiocarlesso/stack/StackFunctions.java
+++ src/main/java/com/fabiocarlesso/stack/StackFunctions.java
@@ -34,4 +34,26 @@ public class StackFunctions {
         return brackets.isEmpty();
     }
 
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            switch (token) {
+                case "+" -> stack.add(stack.pop() + stack.pop());
+                case "-" -> {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.add(b - a);
+                }
+                case "*" -> stack.add(stack.pop() * stack.pop());
+                case "/" -> {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.add(b / a);
+                }
+                default -> stack.add(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+
 }
