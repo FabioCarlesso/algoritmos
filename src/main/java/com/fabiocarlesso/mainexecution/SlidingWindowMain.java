566f1be3a6857b429f8542cda623a3cebe4339a3
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-10-06 11:32:42 -0300

    refactor(SlidingWindow): Add Best Time to Buy and Sell Stock

--- src/main/java/com/fabiocarlesso/mainexecution/SlidingWindowMain.java
+++ src/main/java/com/fabiocarlesso/mainexecution/SlidingWindowMain.java
@@ -1,10 +1,30 @@
 package com.fabiocarlesso.mainexecution;
 
+import com.fabiocarlesso.slidingwindow.SlidingWindow;
+
+import java.util.Arrays;
+
 public class SlidingWindowMain {
+    private static final String ARRAY_TEXT = "Array: ";
+    public static final String RESULT_CASE_1 = "Result case 1: ";
+    public static final String RESULT_CASE_2 = "Result case 2: ";
+    public static final String RESULT_CASE_3 = "Result case 3: ";
     private SlidingWindowMain(){
         throw new IllegalStateException("Utility class");
     }
     public static void slidingWindowMain(){
         System.out.println("\n\nSliding Window");
+        maxProfitMain();
     }
+
+    private static void maxProfitMain() {
+        System.out.println("\nBest Time to Buy and Sell Stock");
+        int[] prices = new int[]{7,1,5,3,6,4};
+        System.out.println(ARRAY_TEXT + Arrays.toString(prices));
+        System.out.println(RESULT_CASE_1 + SlidingWindow.maxProfit(prices));
+        prices = new int[]{7,6,4,3,1};
+        System.out.println(ARRAY_TEXT + Arrays.toString(prices));
+        System.out.println(RESULT_CASE_2 + SlidingWindow.maxProfit(prices));
+    }
+
 }
--- src/main/java/com/fabiocarlesso/slidingwindow/SlidingWindow.java
+++ src/main/java/com/fabiocarlesso/slidingwindow/SlidingWindow.java
@@ -2,7 +2,23 @@ package com.fabiocarlesso.slidingwindow;
 
 public class SlidingWindow {
 
-    public SlidingWindow(){
+    private SlidingWindow(){
         throw new IllegalStateException("Utility class");
     }
+
+    public static int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+        return maxProfit;
+    }
+
 }
